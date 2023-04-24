01c1b28e38559eec93773ba93bfe119b715cf9f7
Shaun Garwood <shaun.garwood@gmail.com>
2023-04-24 06:12:26 -0600

    added decorators

---
+++ app/decorators/grain_jars_decorator.rb
@@ -0,0 +1,24 @@
+class GrainJarsDecorator < Draper::Decorator
+  delegate_all
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+  def innoculated
+    return '-' if object.innoculated.nil?
+
+    object.innoculated.to_fs(:db)
+  end
+
+  def grain
+    return '-' if object.grain.nil?
+
+    object.grain.titleize
+  end
+end
---
+++ spec/decorators/grain_jars_decorator_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe GrainJarsDecorator do
+end
