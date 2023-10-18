4141d9c61cc911531ecf0b51f1ec7ffd278b3209
Shaun Garwood <shaun.garwood@gmail.com>
2023-10-17 20:44:51 -0600

    simplify grain_jar factory

--- spec/factories/grain_jar.rb
+++ spec/factories/grain_jar.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
   factory :grain_jar do
     sequence(:name) { |n| "Grain Jar #{n}" }
-    innoculated { 1.month.ago }
-    shaken { 1.month.ago + 2.weeks }
-    retired { nil }
+    grain { 'oats' }
   end
 end
 
