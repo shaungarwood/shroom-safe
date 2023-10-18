646e779683bcb6667b1563c319421de07bb3a155
Shaun Garwood <shaun.garwood@gmail.com>
2023-10-17 20:43:35 -0600

    add grain jar model test

--- spec/models/grain_jar_spec.rb
+++ spec/models/grain_jar_spec.rb
@@ -3,5 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe GrainJar, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#shake' do
+    let(:jar) { create(:grain_jar) }
+
+    it 'updates the shaken attribute' do
+      expect(jar.shaken).to be_nil
+      jar.shake
+      expect(jar.shaken).to eq(Date.today)
+    end
+
+    context 'when date is provided' do
+      let(:date) { Date.today - 1.week }
+
+      it 'updates uses that date' do
+        expect(jar.shaken).to be_nil
+        jar.shake(date: date)
+        expect(jar.shaken).to eq(date)
+      end
+    end
+  end
 end
